964bd4a6bd8a5bd3a1ea22af83e8ed4587db3d18
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2023-11-13 20:34:25 +0100

    Front login call. With CORS

--- srcs/front/login/static/login/js/spa.js
+++ srcs/front/login/static/login/js/spa.js
@@ -9,7 +9,23 @@ document.addEventListener("DOMContentLoaded", async function () {
   fetch(url)
     .then((response) => response.text())
     .then((html) => {
-      document.querySelector("main").innerHTML = html;
+      document.querySelector("main").innerHTML = "";
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(html, "text/html");
+      let container = document.querySelector("main");
+
+      Array.from(doc.body.children).forEach((child) => {
+        if (child.nodeType === Node.ELEMENT_NODE && child.tagName !== "SCRIPT")
+          container.appendChild(child.cloneNode(true));
+        else if (child.tagName === "SCRIPT") {
+          let newScript = document.createElement("script");
+
+          if (child.src) newScript.src = child.src;
+          else newScript.textContent = child.textContent;
+
+          container.appendChild(newScript);
+        }
+      });
     })
     .catch((error) => {
       console.error("Error:", error);
