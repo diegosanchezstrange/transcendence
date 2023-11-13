a3c7d39d665cb3153e16e5564d079b18d783292b
mzomeno- <mzomeno-@student.42madrid.com>
2023-11-13 19:25:10 +0100

    little translation test for es-es working just fine, no language options yet

--- srcs/front/front/views.py
+++ srcs/front/front/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
 
 def index(request):
-    return render(request, 'home.html')
+    translated_text = _("text is test")
+    return render(request, 'home.html', { 'test':  translated_text })
