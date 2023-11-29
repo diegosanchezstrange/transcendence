04eb9a4ad788caa537ea16312ca5c52af9e97504
mzomeno- <misaclumsy@gmail.com>
2023-11-29 17:48:14 +0100

    stuck on FATAL:  role postgres does not exist

--- srcs/login/login/models.py
+++ srcs/login/login/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=80)
+    password = models.CharField(max_length=80)
+    is_online = models.BooleanField()
--- srcs/login/src/settings.py
+++ srcs/login/src/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'your_db_name',
+        'USER': 'postgres',
+        'PASSWORD': 'your_db_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
