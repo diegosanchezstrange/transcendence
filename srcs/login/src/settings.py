d27cafba77229577a5db75954a960f75f243eb10
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2023-12-02 19:45:25 +0100

    Models in the tcommons app

--- srcs/login/src/settings.py
+++ srcs/login/src/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'login.apps.LoginConfig',
     'corsheaders',
-    # 'tcommons'
+    'tcommons'
 ]
 
 MIDDLEWARE = [
---
+++ srcs/tcommons/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.username
