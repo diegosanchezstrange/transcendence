715c18ec38b8ca65d8167bb8b1dabd95b230eca3
mzomeno- <mzomeno-@student.42madrid.com>
2023-12-05 22:13:40 +0100

    add id primary key to user table

--- srcs/front/login/models.py
+++ srcs/front/login/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class User(models.Model):
+    id = models.BigAutoField(primary_key=True) 
     username = models.CharField(max_length=21)
     avatar = models.ImageField()
     is_online = models.BooleanField()
