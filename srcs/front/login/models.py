a172c0d74d9ea3868c8e2b9a417a48b2a59d8341
mzomeno- <mzomeno-@student.42madrid.com>
2023-12-06 01:46:53 +0100

    Database test successful!

--- srcs/front/login/models.py
+++ srcs/front/login/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     id = models.BigAutoField(primary_key=True) 
     username = models.CharField(max_length=21)
     avatar = models.ImageField()
-    is_online = models.BooleanField()
+    is_online = models.BooleanField(null=True)
 
 class Friendship(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
