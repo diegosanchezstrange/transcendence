5d457c2e684cbcfd4d42fa364a74641b38f3ef02
mzomeno- <misaclumsy@gmail.com>
2024-01-17 11:11:05 +0100

    add event to upload profile image but its a bad request :'

--- srcs/front/users/static/js/editProfile.js
+++ srcs/front/users/static/js/editProfile.js
@@ -1,9 +1,11 @@
 const username = document.getElementById("profile-username");
-const edit_button = document.getElementById("name-edit-button");
+const edit_username_button = document.getElementById("name-edit-button");
 const end_button = document.getElementById("confirm-name-edit-button");
+const avatar = document.getElementById("user-profile-avatar");
+const edit_avatar_button = document.getElementById("img-edit-button");
 
-edit_button.addEventListener("click", function() {
-  edit_button.style.display = "none";
+edit_username_button.addEventListener("click", function() {
+  edit_username_button.style.display = "none";
   end_button.style.display = "inline";
   username.contentEditable = true;
   username.style.backgroundColor = "rgba(54, 54, 71, 0.711)";
@@ -13,18 +15,83 @@ end_button.addEventListener("click", function() {
   username.contentEditable = false;
   username.style.background = "transparent";
   end_button.style.display = "none";
-  edit_button.style.display = "inline";
+  edit_username_button.style.display = "inline";
 } )
 
-function edit_username(e) {
-  e.preventDefault();
-  let new_username = username.value;
+edit_avatar_button.addEventListener("click", function() {
+  
   let headers = {
     "X-Requested-With": "XMLHttpRequest",
     "Content-Type": "application/json",
   };
+  
+  if (Router.getJwt()) headers["Authorization"] = "Bearer " + Router.getJwt();
+  
+  fetch(USERS_SERVICE_HOST + "/users/upload/", {
+    method: "PUT",
+    headers: headers,
+  })
+})
+
+function edit_username(e) {ƒ
+  e.preventDefault();
+  let new_username = username.value;
+
+  formData.forEach((value, key) => {
+    if (key === "csrfmiddlewaretoken") csrfToken = value;
+    body[key] = value;
+  });
+
+  body = JSON.stringify(body);
+
+  let headers = {
+    "X-CSRFToken": csrfToken,
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
 
   if (Router.getJwt()) headers["Authorization"] = "Bearer " + Router.getJwt();
-}
+
+  fetch(this.getAttribute("action"), {
+    method: this.getAttribute("method"),
+    credentials: "include",
+    headers: headers,
+    body: body,
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      }
+      throw new Error("Network response was not ok.");
+    })
+  }
+
+//   let headers = {
+//     "X-Requested-With": "XMLHttpRequest",
+//     "Content-Type": "application/json",
+//   };
+
+//
+
+//   fetch(this.getAttribute("action"), {
+//     method: "PUT",
+//     // credentials: "include",
+//     headers: headers,
+//   })
+//     .then(async function (response) {
+//       if (!response.ok) {
+//         let res = await response.json();
+//         throw new Error(res["detail"]);
+//       }
+//       return response.json();
+//     })
+//     .then(function (json) {
+//       console.log(json["detail"]);
+//     })
+//     .catch(function (error) {
+//       let container = document.getElementById("profile_username");
+//       addAlertBox("Error: " + error.message, "danger", container);
+//     });
+// }
 
 end_button.addEventListener("submit", edit_username);
