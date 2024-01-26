d6a45fdbd2638f7502a58fa1dfb3aa8cd8dfc62b
Manmartin <manuelmartinlopez02@gmail.com>
2024-01-26 19:26:13 +0100

    Change static file location

--- srcs/front/front/settings.py
+++ srcs/front/front/settings.py
@@ -159,8 +159,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/static'
+STATIC_URL = 'static/front/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = '/static/front'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
