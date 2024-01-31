057437277c4b2d1a082027baab71e52bf9f22b48
Manmartin <manuelmartinlopez02@gmail.com>
2024-01-31 19:38:22 +0100

    Profile page allow user to change name

--- srcs/front/static/js/editProfile.js
+++ srcs/front/static/js/editProfile.js
@@ -7,22 +7,24 @@ const end_button = document.getElementById("confirm-name-edit-button");
 edit_username_button.addEventListener("click", function() {
   edit_username_button.style.display = "none";
   end_button.style.display = "inline";
-  username.contentEditable = true;
+
+  username.removeAttribute("disabled");
   username.style.backgroundColor = "rgba(54, 54, 71, 0.711)";
+  username.focus();
+  username.setSelectionRange(username.value.length, username.value.length);
 } );
 
 end_button.addEventListener("click", function(e) {
   e.preventDefault();
-  username.contentEditable = false;
-  username.style.background = "transparent";
   end_button.style.display = "none";
   edit_username_button.style.display = "inline";
+
+  username.setAttribute("disabled", "");
+  username.style.background = "transparent";
 } )
 
 end_button.addEventListener("click", function(e) {
   e.preventDefault();
-  let new_username = username.value;
-
   let headers = {
     "X-Requested-With": "XMLHttpRequest",
     "Content-Type": "application/json",
@@ -30,9 +32,15 @@ end_button.addEventListener("click", function(e) {
 
   if (Router.getJwt()) headers["Authorization"] = "Bearer " + Router.getJwt();
 
+  let body = {
+    "username": username.value
+  }
+  body = JSON.stringify(body);
+
   fetch(USERS_SERVICE_HOST + "/profile/edit/", {
     method: "PUT",
     headers: headers,
+    body: body
   })
     .then((response) => {
       if (response.ok) {
