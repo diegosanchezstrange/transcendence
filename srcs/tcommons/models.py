bb5d118034ae29ab8515aea7fdbf4f3fd4368a54
mzomeno- <misaclumsy@gmail.com>
2024-04-25 19:26:43 +0200

    delete match model

--- srcs/tcommons/models.py
+++ srcs/tcommons/models.py
@@ -8,10 +8,4 @@ class User(models.Model):
 
 class Friendship(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-	friend_of = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend')
-
-class Match(models.Model):
-	player_1 = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='user_1', null=True)
-	player_2 = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='user_2', null=True)
-	winner = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='winner', null=True)
-	date = models.DateField()
+	friend_of = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend')()
